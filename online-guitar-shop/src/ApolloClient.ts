a1f5e78e13a2010cc27a186ee53836ca536ab74e
Festim <krasniqif937@gmail.com>
2025-08-27 16:37:41 +0200

    Updated ApolloClient to use environment variable for URI

--- online-guitar-shop/src/ApolloClient.ts
+++ online-guitar-shop/src/ApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://graphql-api-brown.vercel.app/api/graphql",
-  }),
+    uri: import.meta.env.VITE_GRAPHQL_URI,
+}),
   cache: new InMemoryCache(),
 });
